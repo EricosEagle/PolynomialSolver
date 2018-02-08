92d06aa61a9424e9c174db8787dea93f282cb2b3
Eric G.D <36101457+EricosEagle@users.noreply.github.com>
2018-02-08 18:14:32 +0200

    Update PolynomialSolver.c

--- PolynomialSolver.c
+++ PolynomialSolver.c
@@ -13,9 +13,6 @@
 ==========================================================================*/
 
 
-
-
-
 /*-------------------------------------------------------------------------
 Calculate the roots of a polynomial
  -------------------------------------------------------------------------*/
@@ -23,25 +20,22 @@ int main()
 {
     double a,b,c,root1,root2;
     printf("Enter a polynomial:\n");
-    if (scanf("a=%lf,b=%lf,c=%lf", &a, &b, &c) != 3)
+    if (scanf("a = %lf, b = %lf, c = %lf", &a, &b, &c) != 3) {
+        printf("Error, Invalid input. Please restart the program.");
         return 1;
+    }
     if (b*b < 4*a*c || a == 0)
     {
         printf("There are no roots\n");
         return 0;
     }
-    root1 = (-b + sqrt(b*b - 4*a*c))/2*a;
-    root2 = (-b - sqrt(b*b - 4*a*c))/2*a;
+    double root = sqrt(b*b - 4*a*c);
+    root1 = (-b + root)/2*a;
+    root2 = (-b - root)/2*a;
     if (b*b > 4*a*c)
-    {
         printf("Roots are %f, %f", root1,root2);
-        return 0;
-    }
     else if (b*b == 4*a*c)
-    {
         printf("Root is %f", root1);
-        return 0;
-    }
     return 0;
 
 }
